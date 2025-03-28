87dbfba981fc25c2473038cfed7a706ce6077baf
LuisCortez-ai <munguia.luis0707@gmail.com>
2025-03-28 12:34:53 -0600

    Ejemplo1.py

--- Ejemplos literal D/Ejemplo1.py
+++ Ejemplos literal D/Ejemplo1.py
@@ -1,4 +1,5 @@
 #Ejemplo 1
+#Clase padre
 class CuentadeBitcoin:
     def __init__(self, saldo=0.0):
          self.saldo = saldo
@@ -9,12 +10,23 @@ class CuentadeBitcoin:
          print("\tRetiro exitoso. Nuevo saldo:"+ str(round(self.saldo,2)))
      else: 
         print("\tNo hay suficientes fondos para retirar")
-
-Cuenta1=CuentadeBitcoin(0)
-Cuenta2=CuentadeBitcoin(170.3)
-Cuenta3=CuentadeBitcoin(245.5)
+# Subclase que herada de CuentaDeBitcoin
+class CuentaPremium(CuentadeBitcoin):
+    def __init__(self, saldo=0.0, cashback=0.05):
+        super().__init__(saldo)
+        self.cashback = cashback
+    def retirarCP(self, cantidad):
+        if self.saldo >= cantidad:
+            self.saldo -= cantidad
+            cashback_ganado = cantidad * self.cashback
+            self.saldo += cashback_ganado
+            print(f"\tRetiro exitosos con cashback. Nuevo saldo:"+ str(round(self.saldo,2))+ f" (Cashback: {cashback_ganado})")
+        else:
+            print("\tNo hay suficientes fondos para retirar")
+            
+Cuenta_normal=CuentadeBitcoin(0)
+Cuenta_gold=CuentaPremium(170.3)
 
 #Imprimiendo valores
-Cuenta1.retirar(50.0)
-Cuenta2.retirar(50.0)
-Cuenta3.retirar(50.0)
+Cuenta_normal.retirar(50.0)
+Cuenta_gold.retirarCP(50.0)
